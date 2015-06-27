466c703f7c6f217fafa61203ab2d90cc4f50f626
Kyle Burnett <kyletburnett@gmail.com>
2015-06-27 13:52:24 -0400

    Getting rid of warning when building

--- src/clipper.cpp
+++ src/clipper.cpp
@@ -2166,12 +2166,14 @@ void Clipper::IntersectEdges(TEdge *e1, TEdge *e2,
       }
     }
 
-    if (e1stops)
+    if (e1stops) {
       if (e1->OutIdx < 0) DeleteFromAEL(e1);
       else throw clipperException("Error intersecting polylines");
-    if (e2stops) 
+    }
+    if (e2stops) {
       if (e2->OutIdx < 0) DeleteFromAEL(e2);
       else throw clipperException("Error intersecting polylines");
+    }
     return;
   }
 #endif
